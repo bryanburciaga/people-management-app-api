f0119ce0f030ce958121ebab2a7323908189a316
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-09-20 13:32:27 -0600

    advances

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // require dependencies 
 const express = require('express')
 const mongoose = require('mongoose');
-const logeer = require('morgan');
+const logger = require('morgan');
 // initialize the express app
 const app = express();
 // configuring applications settings
@@ -18,7 +18,8 @@ mongoose.connection
 // set up our model
 const peopleSchema = new mongoose.Schema({
     name: String,
-    image: String,
+    image: {type: String,
+            default: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'},
     title: String
 }, {timestamps: true});
 
@@ -35,6 +36,55 @@ app.use(logger('dev'));
 app.get('/', (req, res) => {
     res.send('Welcome to the People Managment App');
 });
+
+// Full CRUD ROUTES
+
+//INDEX
+app.get('/api/people', async (req, res) => {
+    try {
+        res.status(200).json(await People.find({}))
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({'error': 'bad request'});
+    }
+});
+
+// CREATE Route
+app.post('/api/people', async (req, res) => {
+    try {
+        res.status(201).json(await People.create(req.body));
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({'error': 'bad request'});
+    }
+});
+
+// UPDATE Route
+app.put('/api/people/:id', async (req, res) => {
+    try {
+        res.status(200).json(await People.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        ));     
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({'error': 'bad request'});
+    }
+});
+
+// DELETE route
+app.delete('/api/people/:id', async (req, res) => {
+    try {
+        res.status(200).json(await People.findByIdAndDelete(
+            req.params.id
+        ));
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({'error': 'bad request'});
+    }
+});
+
 // tell the app to listen
 app.listen(PORT, () => {
     console.log(`Express is listening on port: ${PORT}`);
