ff19d4557967fcaf8a5257c882978a51cb8a1b3c
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-09-20 12:26:27 -0600

    defining a model

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // require dependencies 
 const express = require('express')
 const mongoose = require('mongoose');
+const logeer = require('morgan');
 // initialize the express app
 const app = express();
 // configuring applications settings
+
 require('dotenv').config();
 const { PORT = 4000, DATABASE_URL } = process.env;
 // esablish connection to mongodb
@@ -13,7 +15,21 @@ mongoose.connection
 .on('connected', () => console.log('Connected to MongoDB'))
 .on('error', (error) => console.log('Problem with MongoDB' + error.message));
 
+// set up our model
+const peopleSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    title: String
+}, {timestamps: true});
+
+const People = mongoose.model('People', peopleSchema);
+
 // mount middleware
+
+// app.use(express.urlencoded({ extended: false}));
+app.use(express.json()); 
+// this middlewate inctercepts incoming json request bodies and turns sthem into req.body
+app.use(logger('dev'));
 
 // mount our routes
 app.get('/', (req, res) => {
